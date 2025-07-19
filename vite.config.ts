dc2bd892d58d9663fab051d14b069df7e26fe4b6
jharmony <tharealjharmony@gmail.com>
2025-07-19 19:40:15 -0400

    Setup auto-deployment with demo subdomain and all recent updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
     }
   }
 })
